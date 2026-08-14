eea917626463465097521557515c92b1655fd178
Rodrigo Grande <96559980+rodrigogz64@users.noreply.github.com>
2026-08-14 00:03:14 -0600

    Mejorar Inicio y Acerca de con Bootstrap, HTML5 semantico y CSS3

--- obj/Debug/net10.0/BibliotecaMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/BibliotecaMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f740987fed7da2b8afe5cb010e9a7396a23cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73087ec90c7db326d5d80fa105805bc00841afca")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
